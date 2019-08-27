bd2eaf298ec92641119d7761c32a8085a440000d
Belkacem <belkacem.bougueham@themoocagency.com>
2019-08-27 19:53:22 +0200

    Mise ajour snackbar

--- src/components/SnackBarError.js
+++ src/components/SnackBarError.js
@@ -2,13 +2,34 @@ import React from 'react';
 import Snackbar from '@material-ui/core/Snackbar';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
-
+import { amber } from '@material-ui/core/colors';
+import { makeStyles } from '@material-ui/core/styles';
+import WarningIcon from '@material-ui/icons/Warning';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+import clsx from 'clsx';
 export default function SnackBarError(props) {
-
+   
+    const useStyles1 = makeStyles(theme => ({
+        warning: {
+            backgroundColor: amber[700],
+        },
+        icon: {
+            fontSize: 20,
+        },
+        iconVariant: {
+            opacity: 0.9,
+            marginRight: theme.spacing(1),
+        },
+        message: {
+            display: 'flex',
+            alignItems: 'center',
+        },
+    }));
+    const classes = useStyles1();
     return (
         <div>
             <Snackbar
-                className={"warningSnack"}
+                className={classes.warning}
                 anchorOrigin={{
                     vertical: 'bottom',
                     horizontal: 'left',
@@ -23,23 +44,31 @@ export default function SnackBarError(props) {
                         }
                     }
                 }
-                autoHideDuration={6000}
+                autoHideDuration={3000}
+            >
+                <SnackbarContent
+                className={classes.warning}
                 ContentProps={{
-                    'aria-describedby': 'message-id',
+                    'aria-describedby': 'Error-Uid',
                 }}
-                message={<span id="message-id">Id Inexistant</span>}
+                message={<span id="client-snackbar">
+                    <WarningIcon className={clsx(classes.icon, classes.iconVariant)} />
+                    Id inexistant
+                </span>}
                 action={[
                     <IconButton
+                        color="inherit" 
                         key="close"
                         aria-label="close"
                         color="inherit"
                         className={'close'}
                         onClick={props.handleClose}
                     >
-                        <CloseIcon />
+                        <CloseIcon className={classes.icon}/>
                     </IconButton>,
                 ]}
-            />
+                />
+           </Snackbar>
         </div>
     );
 }
